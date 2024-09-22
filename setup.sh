3e2adfc23f2bd726aba3ee3e186d9ec92d97a67d
hitsumabushi845 <shimada.kento8974@gmail.com>
2024-09-23 03:08:57 +0900

    Add yq install

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ bundle install
 rm -rf node_modules
 # --unsafe-perm はrootでの実行時(= docker環境)で必要 非root時の挙動に影響なし
 npm install --unsafe-perm
+
+wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq
+chmod +x /usr/bin/yq
