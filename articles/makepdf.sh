3ae5841e5aa3b55069196a1726fa2ed8218313ca
hitsumabushi845 <shimada.kento8974@gmail.com>
2024-09-23 03:23:43 +0900

    Update makepdf.sh

--- articles/makepdf.sh
+++ articles/makepdf.sh
@@ -3,7 +3,8 @@
 echo "Create PDF..."
 review-pdfmaker config.yml
 echo "Create Gray-scaled PDF..."
-gs -q -r600 -dNOPAUSE -sDEVICE=pdfwrite -o MkDocs-Book-GrayScale.pdf -dPDFSETTINGS=/prepress -dOverrideICC -sProcessColorModel=DeviceGray -sColorConversionStrategy=Gray -sColorConversionStrategyForImage=Gray -dGrayImageResolution=600 -dMonoImageResolution=600 -dColorImageResolution=600 MkDocs-Book.pdf 
+bookname_base=$(yq '.bookname' ./config.yml)
+gs -q -r600 -dNOPAUSE -sDEVICE=pdfwrite -o "$bookname_base"-GrayScale.pdf -dPDFSETTINGS=/prepress -dOverrideICC -sProcessColorModel=DeviceGray -sColorConversionStrategy=Gray -sColorConversionStrategyForImage=Gray -dGrayImageResolution=600 -dMonoImageResolution=600 -dColorImageResolution=600 "$bookname_base".pdf
 
 echo "Create Ebook..."
 review-pdfmaker config-ebook.yml
