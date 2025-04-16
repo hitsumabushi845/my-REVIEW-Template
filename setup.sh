f024a735d0b65f4c8ac284ab87cfb659d44219cd
hitsumabushi845 <shimada.kento8974@gmail.com>
2025-04-16 13:28:19 +0900

    update script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,12 @@
 
 set -eux
 
+apt-get update && apt-get install -y \
+build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev python3-pip
+
 bundle install
 rm -rf node_modules
 # --unsafe-perm はrootでの実行時(= docker環境)で必要 非root時の挙動に影響なし
 npm install --unsafe-perm
+
+pip install --break-system-packages pygments
